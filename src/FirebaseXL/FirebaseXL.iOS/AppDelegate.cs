520b3e9a96f7b02873dbffd79278132bc5204353
Carlos Campos <ccampos@xinobistudio.com>
2018-01-22 13:35:06 -0600

    Added Google Service info plist and Firebase Component

--- src/FirebaseXL/FirebaseXL.iOS/AppDelegate.cs
+++ src/FirebaseXL/FirebaseXL.iOS/AppDelegate.cs
@@ -1,20 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using FirebaseXL.Forms;
 using Foundation;
 using UIKit;
+using Firebase.CloudMessaging;
+using UserNotifications;
 
 namespace FirebaseXL.iOS
 {
     [Register("AppDelegate")]
-    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate, IUNUserNotificationCenterDelegate, IMessagingDelegate
     {
+        public void DidRefreshRegistrationToken(Messaging messaging, string fcmToken)
+        {
+            System.Diagnostics.Debug.WriteLine($"FCM Token: {fcmToken}");
+        }
+
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
 
-            LoadApplication(new App());
+            Firebase.Core.App.Configure();
+
+            LoadApplication(application: new FirebaseXL.Forms.App());
+
+            // Register your app for remote notifications.
+            if (UIDevice.CurrentDevice.CheckSystemVersion(10, 0))
+            {
+                // iOS 10 or later
+                var authOptions = UNAuthorizationOptions.Alert | UNAuthorizationOptions.Badge | UNAuthorizationOptions.Sound;
+                UNUserNotificationCenter.Current.RequestAuthorization(authOptions, (granted, error) => {
+                    Console.WriteLine(granted);
+                });
+
+                // For iOS 10 display notification (sent via APNS)
+                UNUserNotificationCenter.Current.Delegate = this;
+
+                // For iOS 10 data message (sent via FCM)
+                Messaging.SharedInstance.RemoteMessageDelegate = this;
+            }
+            else
+            {
+                // iOS 9 or before
+                var allNotificationTypes = UIUserNotificationType.Alert | UIUserNotificationType.Badge | UIUserNotificationType.Sound;
+                var settings = UIUserNotificationSettings.GetSettingsForTypes(allNotificationTypes, null);
+                UIApplication.SharedApplication.RegisterUserNotificationSettings(settings);
+            }
+
+            UIApplication.SharedApplication.RegisterForRemoteNotifications();
 
             return base.FinishedLaunching(app, options);
         }
