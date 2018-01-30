cd08684d60d088779faffca094866724a1a8350c
Carlos Campos <ccampos@xinobistudio.com>
2018-01-29 22:01:51 -0600

    Added FCM Support for Android

---
+++ src/FirebaseXL/FirebaseXL.Droid/FirebaseService.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.Util;
+using Firebase.Iid;
+
+namespace FirebaseXL.Droid
+{
+    [Service]
+    [IntentFilter(new [] { "com.google.firebase.INSTANCE_ID_EVENT" })]
+    public class FirebaseService : FirebaseInstanceIdService
+    {
+        const string TAG = "MyFirebaseIIDService";
+
+        public override void OnTokenRefresh()
+        {
+            var refreshedToken = FirebaseInstanceId.Instance.Token;
+            Log.Debug(TAG, "Refreshed token: " + refreshedToken);
+            SendRegistrationToServer(refreshedToken);
+        }
+
+        void SendRegistrationToServer(string token)
+        {
+            // Add custom implementation, as needed.
+        }
+    }
+}
--- src/FirebaseXL/FirebaseXL.Droid/MainActivity.cs
+++ src/FirebaseXL/FirebaseXL.Droid/MainActivity.cs
@@ -6,7 +6,10 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
-using FirebaseXL.Forms;
+using Firebase.Messaging;
+using Firebase.Iid;
+using Android.Util;
+using Android.Gms.Common;
 
 namespace FirebaseXL.Droid
 {
@@ -22,7 +25,28 @@ namespace FirebaseXL.Droid
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
 
-            LoadApplication(new App());
+            LoadApplication(new FirebaseXL.Forms.App());
+
+            CheckForGoogleServices();
+        }
+
+        // Check if the Google Play Services is available to recieve Push Notification from Firebase
+        public bool CheckForGoogleServices()
+        {
+            int resultCode = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable(this);
+            if (resultCode != ConnectionResult.Success)
+            {
+                if (GoogleApiAvailability.Instance.IsUserResolvableError(resultCode))
+                {
+                    Toast.MakeText(this, GoogleApiAvailability.Instance.GetErrorString(resultCode), ToastLength.Long);
+                }
+                else
+                {
+                    Toast.MakeText(this, "This device does not support Google Play Services", ToastLength.Long);
+                }
+                return false;
+            }
+            return true;
         }
     }
 }
